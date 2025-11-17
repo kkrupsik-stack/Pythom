175885bae131037c0d01a9b68441fc30e5aa9860
Дробик и ПименовИСП1515 <kkrupsik@gmail.com>
2025-11-17 11:45:44 +0300

    Create Задание 2.py

---
+++ Задание 2.py
@@ -0,0 +1,13 @@
+# Задание 2
+
+path = "c:\\Users\\MainAdmin\\Desktop\\programs"
+print(path)
+path += "\\game.exe"
+print(path)
+path = "c:\\Users\\MainAdmin\\Desktop\\files"
+path += "\\picture.png"
+print(path)
+path = "c:\\Games\\city simulator"
+R = 3
+path = path * R
+print(f"Error {path}.")
