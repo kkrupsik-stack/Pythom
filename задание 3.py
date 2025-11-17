72d5c612c25761c65a39b0eae16a5bb695d1240c
Дробик и ПименовИСП1515 <kkrupsik@gmail.com>
2025-11-17 12:24:21 +0300

    Create задание 3.py

---
+++ задание 3.py
@@ -0,0 +1,24 @@
+# Задание
+num_1 = 7
+num_2 = 10
+num_3 = 4
+sum_result = num_1 + num_2 + num_3
+print("Сумма всех целых чисел:", sum_result)
+num_1 = 7.9
+num_3 = float(num_3)  
+sum_result2 = num_1 + num_2 + num_3
+print("Сумма всех цецелих чисел:", sum_result2)
+num_1 = 130
+num_2 = 4
+num_3 = 2
+multiplying = num_1 * num_2 * num_3
+print("Умножение всех чисел:", multiplying)
+division = (num_1 / num_2) / num_3
+print("Деление:", division)
+num_1 = 2
+num_2 = 3
+num_3 = 4
+degree = num_1  (num_2  num_3)
+print("Числа в степени:", degree)
+math_result = ((43 + num_1) + (num_2 + 67) - (num_3 * 2))
+print(math_result)
