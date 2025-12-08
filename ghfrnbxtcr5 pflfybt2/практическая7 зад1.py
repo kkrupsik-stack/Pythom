2d711afe1241d18c2a297491c0be0b79a8f6282f
Дробик и ПименовИСП1515 <kkrupsik@gmail.com>
2025-12-08 12:36:29 +0300

    Create практическая7 зад1.py

---
+++ ghfrnbxtcr5 pflfybt2/практическая7 зад1.py
@@ -0,0 +1,11 @@
+array_2d = [
+    ['folder', 'coursework.doc', 'folder', 'pict.png', 'data.accdb'],
+    ['icon.ico', 'script.js', 'index.html', 'style.css', 'prog.py'],
+    ['my_song.mp3', 'anapa-2003.jpg', 'cs 1.6.exe', 'folder', 'cheat.txt'],
+    ['notes.txt', 'main.py', 'work.pdf', 'cartoon.mp4', 'array.py'],
+    ['project.psd', 'cycle.py', 'folder', 'cycle.js', 'turtle.py']
+]
+
+print("Начальный список:")
+for row in array_2d:
+    print(row)
