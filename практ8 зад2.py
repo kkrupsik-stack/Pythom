bcd2c272ddf85737f776483c05ed7971b689022a
Дробик и ПименовИСП1515 <kkrupsik@gmail.com>
2025-12-11 09:29:24 +0300

    Create практ8 зад2.py

---
+++ практ8 зад2.py
@@ -0,0 +1,11 @@
+
+sl = {'</>': 13, 'script': 1, '_init_': 10, 'self': 5, 'number_9': 6, '#comment': 100}
+
+print(sl)
+
+key = input('key: ')  
+value = input('value: ')  
+
+sl[key] = value
+
+print(sl)
